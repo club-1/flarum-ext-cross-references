95411535e2bb86884560749fba57b66ac4408c7f
Nicolas Peugnet <nicolas@club1.fr>
2023-02-03 17:53:09 +0100

    Add tests for CROSSREFERENCEURL strings
    + some refactoring.

--- tests/unit/CrossReferencesConfiguratorTest.php
+++ tests/unit/CrossReferencesConfiguratorTest.php
@@ -30,7 +30,9 @@ use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\Testing\unit\TestCase;
 use Flarum\User\User;
 use Mockery as m;
-use s9e\TextFormatter;
+use s9e\TextFormatter\Configurator;
+use s9e\TextFormatter\Parser;
+use s9e\TextFormatter\Utils;
 
 use function PHPUnit\Framework\assertEquals;
 
@@ -40,7 +42,7 @@ class CrossReferencesConfiguratorTest extends TestCase
     protected $settingsRepo;
     /** @var UrlGenerator */
     protected $urlGenerator;
-    /** @var TextFormatter\Configurator */
+    /** @var Configurator */
     protected $configurator;
 
     public function setUp(): void
@@ -57,13 +59,13 @@ class CrossReferencesConfiguratorTest extends TestCase
 
         // Mock Eloquent Discussion Model by creating an alias in the autoloader.
         // This only works if the aliased class is not yet loaded.
-        // See: <http://docs.mockery.io/en/latest/reference/creating_test_doubles.html#aliasing>
+        // See: <https://docs.mockery.io/en/latest/reference/creating_test_doubles.html#aliasing>
         $discussionModel = m::mock('alias:Flarum\Discussion\Discussion');
         $discussion = new \Flarum\Discussion\Discussion();
         $discussion->title = 'dummy discussion';
         $discussionModel->shouldReceive('find')->andReturn($discussion);
 
-        $this->configurator = new TextFormatter\Configurator;
+        $this->configurator = new Configurator;
     }
 
     public static function mockActor(bool $allowed = true): User
@@ -73,6 +75,15 @@ class CrossReferencesConfiguratorTest extends TestCase
         return $actor;
     }
 
+    public function basicParser(): Parser
+    {
+        $xrefConfigurator = new CrossReferencesConfigurator($this->settingsRepo, $this->urlGenerator);
+        $xrefConfigurator($this->configurator);
+        extract($this->configurator->finalize());
+        $parser->registeredVars['actor'] = self::mockActor(true);
+        return $parser;
+    }
+
     /**
      * @dataProvider shortReferencesProvider
      * @param string $text The text to parse.
@@ -80,13 +91,9 @@ class CrossReferencesConfiguratorTest extends TestCase
      */
     public function testShortReferences(string $text, array $expected): void
     {
-        $xrefConfigurator = new CrossReferencesConfigurator($this->settingsRepo, $this->urlGenerator);
-        $xrefConfigurator($this->configurator);
-        extract($this->configurator->finalize());
-        $parser->registeredVars['actor'] = self::mockActor(true);
-
+        $parser = $this->basicParser();
         $xml = $parser->parse($text);
-        $actual = TextFormatter\Utils::getAttributeValues($xml, 'CROSSREFERENCESHORT', 'id');
+        $actual = Utils::getAttributeValues($xml, 'CROSSREFERENCESHORT', 'id');
         assertEquals($expected, $actual);
     }
 
@@ -102,6 +109,44 @@ class CrossReferencesConfiguratorTest extends TestCase
             ['?(#3234', ['3234']],
             [' #7', ['7']],
             ['#8.', ['8']],
+        ];
+    }
+
+    /**
+     * @dataProvider urlReferencesProvider
+     * @param string $text The text to parse.
+     * @param string[] $expected The expected array of ids found in the text.
+     */
+    public function testUrlReferences(string $text, array $expected): void
+    {
+        $parser = $this->basicParser();
+        $xml = $parser->parse($text);
+        $actual = Utils::getAttributeValues($xml, 'CROSSREFERENCEURL', 'id');
+        assertEquals($expected, $actual);
+    }
+
+    public function urlReferencesProvider(): array
+    {
+        return [
+            ['https://forum.club1.fr/d/3-discussion-3/foo.', []],
+            ['https://forum.club1.fr/d/3-discussion-3/,foo', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-/foo.', []],
+            ['https://forum.club1.fr/d/3-discussion-3/ ', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-3/}', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-3 ', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-3/.', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-3//', []],
+            ['https://forum.club1.fr/d/3-discussion-3.', ['3']],
+            ['https://forum.club1.fr/d/3-discussion-.', ['3']],
+            ['https://forum.club1.fr/d/3-discussion', ['3']],
+            ['https://forum.club1.fr/d/3-', ['3']],
+            ['https://forum.club1.fr/d/3discussion', []],
+            ['https://forum.club1.fr/d/3', ['3']],
+            [' https://forum.club1.fr/d/3', ['3']],
+            ['coucou https://forum.club1.fr/d/3', ['3']],
+            ['coucou phttps://forum.club1.fr/d/3', []],
+            ['coucou.https://forum.club1.fr/d/3', ['3']],
+            ['coucou (https://forum.club1.fr/d/3)', ['3']],
         ];
     }
 }

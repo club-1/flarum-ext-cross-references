1d79290a766b98c515256345591dc26bc96e415e
Nicolas Peugnet <nicolas@club1.fr>
2023-02-18 13:27:53 +0100

    WIP: proff of concept of bufferized store find

--- js/src/forum/cache.ts
+++ js/src/forum/cache.ts
@@ -76,11 +76,79 @@ ModelMap[Discussion.name] = 'discussions';
 /** Empty function used to disable callbacks */
 function noop() {};
 
+type ResolveFunc<T> = (value: T[] | PromiseLike<T[]>) => void;
+type RejectFunc = (reason?: any) => void;
+const BUFFER_SIZE = 10;
+const BUFFER_TIMEOUT_MS = 30;
+
+class RequestsBuffer<T extends Model> {
+  protected type: string;
+  protected requests: Map<string, Promise<T | null>> = new Map;
+
+  protected timer?: ReturnType<typeof setTimeout>;
+  protected request?: Promise<T[]>;
+  protected flushFunc?: () => void;
+
+  constructor(type: string) {
+    this.type = type;
+  }
+
+  public push(id: string): Promise<T | null> {
+    let req = this.requests.get(id);
+    if (req) {
+      return req;
+    }
+    if (!this.request) {
+      this.request = this.prepareRequest();
+    }
+    req = this.request.then((res) => {
+      return res.find((obj) => obj.id() == id) || null
+    });
+    this.requests.set(id, req);
+    if (this.requests.size == BUFFER_SIZE) {
+      this.flushNow();
+    }
+    return req;
+  }
+
+  protected prepareRequest(): Promise<T[]> {
+    return new Promise<T[]>((resolve, reject) => {
+      this.flushFunc = () => this.flushBuffer(resolve, reject);
+      this.timer = setTimeout(this.flushFunc, BUFFER_TIMEOUT_MS)
+    });
+  }
+
+  protected flushNow(): void {
+    clearTimeout(this.timer);
+    this.flushFunc!();
+  }
+
+  protected flushBuffer(resolve: ResolveFunc<T>, reject: RejectFunc) {
+    const ids = Array.from(this.requests.keys());
+    this.requests = new Map;
+    delete this.timer;
+    delete this.request;
+    delete this.flushFunc;
+
+    app.store.find<T[]>(ModelMap[this.type], ids, {}, {errorHandler: noop})
+      .catch(reject)
+      .then((res) => {
+        if (!res) {
+          reject();
+          return [];
+        }
+        resolve(res)
+        return res;
+      });
+  }
+};
+
 export abstract class ResponseCache {
+  private static requestsBuffers: Map<string, RequestsBuffer<Model>> = new Map;
   private static responseErrors: Cache<boolean> = new FIFOCache(128);
   private static inFlight: Map<string, Promise<Model | null>> = new Map();
 
-  public static async find<T extends Model>(m: new () => T, id: string, options = {}): Promise<T | null> {
+  public static async find<T extends Model>(m: new () => T, id: string): Promise<T | null> {
     const key = getKey(m.name, id);
 
     if (this.responseErrors.get(key) == true) {
@@ -89,15 +157,22 @@ export abstract class ResponseCache {
     const inFlight = this.inFlight.get(key);
     if (inFlight) {
       return inFlight as Promise<T | null>;
-    } else {
-      const req = app.store.find<T>(ModelMap[m.name], id, options, {errorHandler: noop})
-        .catch(() => {
-          this.responseErrors.set(key, true);
-          return null;
-        })
-        .finally(() => this.inFlight.delete(key));
-      this.inFlight.set(key, req);
-      return req;
     }
+
+    let buffer = this.requestsBuffers.get(m.name);
+    if (!buffer) {
+      buffer = new RequestsBuffer<T>(m.name);
+      this.requestsBuffers.set(m.name, buffer);
+    }
+    const req = buffer.push(id) as Promise<T | null>
+    this.inFlight.set(key, req);
+    return req
+      .then((res) => {
+        if (!res) {
+          this.responseErrors.set(key, true);
+        }
+        return res;
+      })
+      .finally(() => this.inFlight.delete(key));
   }
 }
